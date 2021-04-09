f1fa891c67939baf81de3ce9371ad00cbe078694
Fe_Aliaksei <---@ddd.w>
2021-04-09 15:07:31 +0300

    English locale support added.

--- lib/interactor/ResourceInteractor.dart
+++ lib/interactor/ResourceInteractor.dart
@@ -1,13 +1,18 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
-
+import 'dart:ui' as ui;
 class ResourceInteractor {
-  static const _localizedStringsFileName = "assets/strings/ru.json";
+  static const _localizedStringsFileNameRu = "assets/strings/ru.json";
+  static const _localizedStringsFileNameEn = "assets/strings/en.json";
   Map localizedStrings;
 
   Future<bool> init(BuildContext context) async {
+    final stringResourcesFileName = ui.window.locale.languageCode.toLowerCase().contains("ru")
+      ? _localizedStringsFileNameRu
+      : _localizedStringsFileNameEn;
+
     var rawStrings = await DefaultAssetBundle.of(context)
-      .loadString(_localizedStringsFileName);
+      .loadString(stringResourcesFileName);
     localizedStrings = await json.decode(rawStrings) as Map;
     return Future<bool>.delayed(Duration(milliseconds: 500),() => true,); //Just for debug )))
   }
